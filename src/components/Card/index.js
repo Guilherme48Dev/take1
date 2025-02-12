cd9d0a2058ef1142f2d18d6b6fab593038945445
Guilherme48Dev <guilhermesouzadjesusdev@gmail.com>
2025-02-12 02:51:39 -0300

    detalhes css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,23 +6,32 @@ import { Link } from "react-router-dom";
 
 function Card({ id, titulo, capa }) {
   const { favorito, adicionarFavorito } = useFavoritoContext();
-  const ehFavorito = favorito.some((fav) => fav.id ===id);
+  const ehFavorito = favorito.some((fav) => fav.id === id);
   const icone = ehFavorito ? iconeFavoritar : iconeDesfavoritar;
 
   return (
     <div className={styles.container}>
       <Link className={styles.link} to={`/${id}`}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
+        <img src={capa} alt={titulo} className={styles.capa} />
+        <h2>{titulo}</h2>
       </Link>
-      <img
-        src={icone}
-        alt="Favoritar filme"
-        className={styles.favoritar}
-        onClick={() => {
-          adicionarFavorito({ id, titulo, capa });
-        }}
-      />
+      <div className={styles.favorito}>
+        <img
+          src={icone}
+          alt="Favoritar filme"
+          className={styles.favoritar}
+          onClick={() => {
+            adicionarFavorito({ id, titulo, capa });
+          }}
+        />
+        <p
+          onClick={() => {
+            adicionarFavorito({ id, titulo, capa });
+          }}
+        >
+          Salvar
+        </p>
+      </div>
     </div>
   );
 }
